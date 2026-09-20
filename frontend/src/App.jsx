9e89ba89e967c42cfec7ea12facf51562827f9ec
adrian-glez-baamonde <adrian.glez.baamonde@gmail.com>
2026-09-20 12:23:52 +0200

    feat: protege rutas home y stats contra acceso sin autenticación

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,35 @@
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import LoginPage from './pages/LoginPage'
-import HomePage from './pages/HomePage'
-import StatsPage from './pages/StatsPage'
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import StatsPage from "./pages/StatsPage";
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/stats" element={<StatsPage />} />
+        <Route
+          path="/stats"
+          element={
+            <ProtectedRoute>
+              <StatsPage />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
