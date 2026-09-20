a8d789bc7524cb6d4ad408e5b1016a4e161c60d1
adrian-glez-baamonde <adrian.glez.baamonde@gmail.com>
2026-09-20 11:47:41 +0200

    feat: configura CORS en el backend y conecta HomePage con GET /books

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,12 +2,22 @@ from fastapi import FastAPI
 from app.routers import auth, books
 from app.database import engine, Base
 from app.models import User, Book
+from fastapi.middleware.cors import CORSMiddleware
 
 
 Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(auth.router)
